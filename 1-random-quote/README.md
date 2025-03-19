ce7dae0c094bbd657b0046539fcbb04a6abeb049
Gurudeep Pandey <pandeygurudeep@gmail.com>
2025-03-20 01:23:38 +0530

    complete random quote project

---
+++ 1-random-quote/README.md
@@ -0,0 +1,27 @@
+# Quote of the Day
+
+## Application Features
+- **Fetch Random Quotes**: Retrieves a random quote from an API.
+- **Display Background Image**: Fetches a random grayscale image as the background for the quote.
+- **Copy to Clipboard**: Allows users to copy the displayed quote.
+- **Download Quote Image**: Users can download the quote as an image.
+- **Share on Twitter**: Easily share the quote on Twitter.
+
+## Screenshots
+![Screenshot](./Images/screenshot.png)
+
+## Deployment Link
+[Live Demo](https://new-random-quote.netlify.app)
+
+
+## Technologies Used
+- HTML
+- CSS
+- JavaScript
+- Fetch API
+- HTML2Canvas
+
+## Credits
+- Quotes API: [FreeAPI](https://api.freeapi.app/)
+- Images: [Picsum Photos](https://picsum.photos/)
+
--- 1-random-quote/script.js
+++ 1-random-quote/script.js
@@ -1,11 +1,11 @@
+// collecting all references
 const img_cont = document.querySelector(".img-cont");
 const quote_div = document.querySelector("#quote");
 const author_div = document.querySelector("#author");
-const btn = document.querySelector("#btn");
+const newQBtn = document.querySelector("#newBtn");
 const copyBtn = document.querySelector("#copyBtn");
-const downloadBtn = document.querySelector("#downloadBtn");
-let funcExecuted = false
 
+// function to fetch quote
 async function getQuote() {
     try {
         const response = await fetch("https://api.freeapi.app/api/v1/public/quotes/quote/random")
@@ -14,54 +14,79 @@ async function getQuote() {
             "quote": quote.data.content,
             "author": quote.data.author
         }
-
     } catch (error) {
         console.log("Error in fetching quote: ", error)
-        return { "quote": "Failed to fetch quote", "author": "unknown" }
+        // fallback quote
+        return { "quote": "There is no fear for one whose mind is not filled with desires.", "author": "The Buddha" }
     }
 }
 
+// function to fetch image
 async function getImageUrl() {
     try {
-        const response = await fetch("https://picsum.photos/400")
-        console.log(response)
-        response.bodyUsed = true
-        console.log(response.bodyUsed)
-        return { "image_url": response.url, "ReadableStream": response.body };
+        const response = await fetch("https://picsum.photos/1000/400?grayscale")
+        const blob = await response.blob();
+        const imageUrl = URL.createObjectURL(blob)
+        return imageUrl;
     } catch (error) {
         console.log("Error in fetching image: ", error)
-        return "./images/default.jpg"
+        // fallback image
+        return "./Images/default.jpeg"
     }
 }
 
+// function that show quote and image on screen
 async function displayQuoteAndImage() {
     try {
         const result = await getQuote()
-        const { image_url, ReadableStream } = await getImageUrl()
+        const image_url = await getImageUrl()
         img_cont.style.backgroundImage = `url("${image_url}")`
         quote_div.innerText = result.quote
-        author_div.innerText = result.author
-
-        // const imageBlog = await ReadableStream.blob()
-        // const imageURL = URL.createObjectURL(imageBlog)
-        downloadBtn.href = image_url
-
+        author_div.innerText = `~ ${result.author}`
     } catch (error) {
         console.log("Error in displaying quote and image: ", error)
     }
 }
+
+// displaying quote and image when page loads
 displayQuoteAndImage()
 
-btn.addEventListener("click", async () => {
-    if (!funcExecuted) {
-        funcExecuted = true
-        await displayQuoteAndImage()
-        funcExecuted = false
-    }
+// new quote button handler
+newQBtn.addEventListener("click", async () => {
+    newQBtn.innerText = "Loading..."
+    newQBtn.disabled = true
+    await displayQuoteAndImage()
+    newQBtn.innerText = "New Quote"
+    newQBtn.disabled = false
 })
 
+// copy button handler
 copyBtn.addEventListener("click", () => {
     navigator.clipboard.writeText(`${quote_div.innerText}`)
-        .then(() => console.log("Text copy to clipboared"))
+        .then(() => {
+            copyBtn.innerText = "Text Copied"
+            setTimeout(() => {
+                copyBtn.innerText = "Copy Quote"
+            }, 1000)
+        })
         .catch((err) => console.log("Error in copying text", err))
+})
+
+// download button handler
+document.querySelector("#downloadBtn").addEventListener("click", () => {
+    html2canvas(img_cont).then((canvas) => {
+        const image = canvas.toDataURL("image/jpg");
+        const link = document.createElement("a");
+        link.href = image;
+        link.download = "quote-screenshot.png";
+        link.click();
+    });
+})
+
+// share button handler
+document.querySelector("#shareBtn").addEventListener("click", () => {
+    window.open(
+        `https://twitter.com/intent/tweet?text=${quote_div.innerText} - ${author_div.innerText}`,
+        "_blank"
+    );
 })
